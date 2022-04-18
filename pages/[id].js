437439ec8f6db366e11e5f71a685951de998c2de
Moinuddin Ahmad <muas130@gmail.com>
2022-04-19 02:17:29 +0600

    practice project completed

---
+++ pages/[id].js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabase";
+import Video from "react-player";
+import { useUser } from "../context/user";
+
+const LessonDetails = ({ lesson }) => {
+  const { user } = useUser();
+  const [videoUrl, setVideoUrl] = useState();
+
+  const getPremiumContent = async () => {
+    const { data } = await supabase
+      .from("premium_content")
+      .select("video_url")
+      .eq("id", lesson.id)
+      .single();
+
+    setVideoUrl(data?.video_url);
+  };
+
+  useEffect(() => {
+    getPremiumContent();
+  });
+
+  const showVideo = !!user && user.is_subscribed;
+
+  return (
+    <div className="w-full max-w-3xl mx-auto py-16 px-8">
+      <h1 className="text-3xl mb-6">{lesson.title}</h1>
+      <p className="mb-8">{lesson.description}</p>
+      {showVideo && <Video url={videoUrl} width="100%" />}
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("id");
+
+  const paths = lessons.map(({ id }) => ({
+    params: {
+      id: id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { id } }) => {
+  const { data: lesson } = await supabase
+    .from("lesson")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  return {
+    props: {
+      lesson,
+    },
+  };
+};
+
+export default LessonDetails;
